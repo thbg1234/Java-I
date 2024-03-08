bd9684eab95c4025e4af843afab2c3ea5cc52dde
thbg1234 <144121222+thbg1234@users.noreply.github.com>
2024-03-08 13:06:18 +0900

    Add files via upload

---
+++ a20240308/first.java
@@ -0,0 +1,16 @@
+package a20240308;
+/*
+ * 작성일:2024.3.8
+ * 작성자:컴퓨터공학부 202195056 석홍신
+ * 설명:첫 번째 java program
+*/
+public class first {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요.");
+		System.out.print("석홍신입니다.");
+		System.out.println("반갑습니다."+"첫 번째 자바 프로그램입니다.");
+		
+	}
+
+}
